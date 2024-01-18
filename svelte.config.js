7e133fe6bd64e4fb0c181f98271c5328ab0518fe
David McCullah <davidwmccullah@gmail.com>
2024-01-17 22:41:14 -0600

    Switched to Auto Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,30 @@
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+// import preprocess from 'svelte-preprocess';
+
+// export default {
+//     kit: {
+//         adapter: adapter({
+//             // default options are shown. On some platforms
+//             // these options are set automatically — see below
+//             pages: 'build',
+//             assets: 'build',
+//             fallback: undefined,
+//             precompress: false,
+//             strict: true
+//         }),
+//     },
+//     preprocess: preprocess(),
+// };
+
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
     kit: {
-        adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
-            fallback: undefined,
-            precompress: false,
-            strict: true
-        }),
+        adapter: adapter()
     },
-    preprocess: preprocess(),
+    preprocess: preprocess()
 };
+
+export default config;
