6deccd8a1021b56a84fd913efc0677c389a3f753
David McCullah <davidwmccullah@gmail.com>
2024-01-17 22:49:45 -0600

    Switched to Node Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@
 //     preprocess: preprocess(),
 // };
 
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
