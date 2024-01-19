4eb9192531e4cfb71f8fdfe872cf599b34c0c0ec
David McCullah <davidwmccullah@gmail.com>
2024-01-18 19:43:39 -0600

    Attempting to add SSL

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
     https: {
       key: readFileSync('/etc/ssl/world-of-whimsy.key'),
       cert: readFileSync('/etc/ssl/world-of-whimsy.pem')
-    }
+    },
+	port: 443
   },
   plugins: [sveltekit()],
 });
