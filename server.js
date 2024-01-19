2945a3f46ccf069ba9cdb3161e16cf0f32ab44d2
David McCullah <davidwmccullah@gmail.com>
2024-01-18 23:49:13 -0600

    SSL again

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
 import { handler } from './build/handler.js';
-// import { https } from 'http';
 import { readFileSync } from 'fs';
 import express from 'express';
+const https = require('node:https');
 
-// const options = {
-//   key: readFileSync('/etc/ssl/world-of-whimsy.key'),
-//   cert: readFileSync('/etc/ssl/world-of-whimsy.pem')
-// };
-// const HOST = '172.232.10.121';
 const PORT = 443;
+
 const app = express();
-
-// add a route that lives separately from the SvelteKit app
-app.get('/healthcheck', (req, res) => {
-	res.end('ok');
-});
-
-// let SvelteKit handle everything else, including serving prerendered pages and static assets
 app.use(handler);
 
-app.listen(PORT, () => {
-	console.log('listening on port ' + PORT);
-});
+https.createServer({
+  key: readFileSync('/etc/ssl/world-of-whimsy.key'),
+  cert: readFileSync('/etc/ssl/world-of-whimsy.pem')
+}, app).listen(PORT); 
