0b939b5daef56b9622916a82f473f0eb40098d7a
David McCullah <davidwmccullah@gmail.com>
2024-01-18 21:02:25 -0600

    Adding slider for Visualizer fftsize

--- src/lib/widgets/Visualizer.svelte
+++ src/lib/widgets/Visualizer.svelte
@@ -430,7 +430,7 @@
 
         <Hexagon class="gap">
             <div class="slider-wrapper">
-                <input type="range" min="5" max="15" step="1" bind:value={fftSizeExp} on:input={(e) => {console.log("here"); updateFFTSize}}/>
+                <input type="range" min="5" max="15" step="1" bind:value={fftSizeExp} on:input={(e) => {console.log("here"); updateFFTSize()}}/>
             </div>
         </Hexagon>
 
