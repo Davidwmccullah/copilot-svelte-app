a0ba535bdddbef12b0671d9a0f82057cb9351c6f
David McCullah <davidwmccullah@gmail.com>
2024-01-18 23:54:56 -0600

    Server logging

--- server.js
+++ server.js
@@ -8,15 +8,7 @@ const PORT = 443;
 const app = express();
 app.use(handler);
 
-const server = https.createServer({
+https.createServer({
   key: readFileSync('/etc/ssl/world-of-whimsy.key'),
   cert: readFileSync('/etc/ssl/world-of-whimsy.pem')
-}, app);
-
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-server.on('listening', () => {
-  console.log('Server started successfully');
-});
+}, app).listen(PORT); 
