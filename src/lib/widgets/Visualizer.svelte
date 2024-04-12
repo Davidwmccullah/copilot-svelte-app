73ee77fe74837fabf3f45e7dc0edf8419c49e557
David McCullah <davidwmccullah@gmail.com>
2024-04-11 22:14:14 -0500

    testing bass function

--- src/lib/widgets/Visualizer.svelte
+++ src/lib/widgets/Visualizer.svelte
@@ -7,9 +7,9 @@
 
     let playlist: string[] = [
         "audio/K3NZH - Still 50 Cent.m4a",
-        "audio/Mechanicus OST.m4a",
-        "audio/Mehdibh - Sekiro.wav",
-        "audio/Darktide - Waiting to Strike + Imperial Strike.m4a",
+        // "audio/Mechanicus OST.m4a",
+        // "audio/Mehdibh - Sekiro.wav",
+        // "audio/Darktide - Waiting to Strike + Imperial Strike.m4a",
     ]
 
     let visualizerWrapper: HTMLDivElement | null = null;
@@ -18,7 +18,7 @@
     let audio: HTMLAudioElement | null = null;
     let audioSrc: any = null;
     let analyser: AnalyserNode | null = null;
-    let fftSizeExp: number = 5; // min is 5, max is 15
+    let fftSizeExp: number = 10; // min is 5, max is 15
     let fftSize: number = Math.pow(2, fftSizeExp); // min is 32, max is 32768
     let dataArray: Uint8Array | null = null;
     let isPlaying: boolean = false;
@@ -34,6 +34,9 @@
     let filled = true;
     let mirrored = true;
     let fileInput: HTMLInputElement | null = null;
+    let smoothStrength: number = .1;
+    let logCutoff: number = 1;
+    let sampleRate: number = 44100;
 
     onMount((): void => {
         initAudio();
@@ -161,6 +164,8 @@
         audioSrc = audioCtx.createMediaElementSource(audio);
         analyser = audioCtx.createAnalyser();
 
+        sampleRate = audioCtx.sampleRate;
+
         audioSrc.connect(analyser);
         analyser.connect(audioCtx.destination);
 
@@ -169,41 +174,84 @@
         isInitialized = true;
     };
 
-    function applyBassRollOff(dataArray) {
-        let filteredData = new Uint8Array(dataArray.length);
-        let alpha = 0.8; // This value can be adjusted to control the amount of smoothing
-        filteredData[0] = dataArray[0];
-        for (let i = 1; i < dataArray.length; i++) {
-            filteredData[i] = alpha * dataArray[i] + (1 - alpha) * filteredData[i - 1];
-        }
-        return filteredData;
-    }
-
     let playVisualizer = (): void => {
         if (!canvas || !canvasCtx || !analyser || !dataArray) return;
 
         canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
         analyser.getByteFrequencyData(dataArray);
       
-        let data: Uint8Array = dataArray;
-        // let data: Uint8Array = applyBassRollOff(dataArray);
-        // let data: Uint8Array = data.map((x) => {return 255});
+        // let data: Uint8Array = dataArray;
+        // Calculate the index range for each frequency band
+        let bands = [
+            {start: 20, end: 60},   // Sub-bass
+            {start: 60, end: 250},  // Bass
+            {start: 250, end: 500}, // Low Midrange
+            {start: 500, end: 2000}, // Midrange
+            {start: 2000, end: 4000}, // Upper Midrange
+            {start: 4000, end: 6000}, // Presence
+            {start: 6000, end: 20000} // Brilliance
+        ].map(band => ({
+            startIndex: Math.round(band.start * (fftSize / sampleRate)),
+            endIndex: Math.round(band.end * (fftSize / sampleRate))
+        }));
 
-        drawRectangles2(data);
+        // get an equal distribution of each set of bands and stitch the arrays together
+        let selectedDataArray = new Uint8Array(bands.reduce((acc, band) => acc + (band.endIndex - band.startIndex), 0));
+
+        let offset = 0;
+        for (let band of bands) {
+            for (let i = band.startIndex; i < band.endIndex; i++) {
+                selectedDataArray[offset] = dataArray[i];
+                offset++;
+            }
+        }
+
+        let data: Uint8Array = lowessSmooth(new Uint8Array(selectedDataArray), smoothStrength);
+            // let data: Uint8Array = dataArray.map((x) => {return 255});
+
+        // drawRectangles(data);
+        drawRegularPolygon(data);
 
         animationFrameId = requestAnimationFrame(function () {
             playVisualizer();
         });
     };
 
-    let drawRectangles2 = (data: Uint8Array): void => {
+    let lowessSmooth = (dataArray: Uint8Array, bandwidth: number): Uint8Array => {
+        let smoothedData = new Array(dataArray.length);
+
+        // Apply the Lowess algorithm to each data point
+        for (let i = 0; i < dataArray.length; i++) {
+            let weights = new Array(dataArray.length).fill(0);
+            let weightSum = 0;
+            for (let j = 0; j < dataArray.length; j++) {
+                let distance = Math.abs(i - j) / dataArray.length;
+                if (distance <= bandwidth) {
+                    let weight = Math.pow(1 - Math.pow(distance / bandwidth, 3), 3);
+                    weights[j] = weight;
+                    weightSum += weight;
+                }
+            }
+
+            let sum = 0;
+            for (let j = 0; j < dataArray.length; j++) {
+                sum += dataArray[j] * weights[j];
+            }
+            smoothedData[i] = sum / weightSum;
+        }
+
+        // Convert the smoothed data to a Uint8Array
+        let result = Uint8Array.from(smoothedData, x => Math.min(255, Math.max(0, Math.round(x))));
+
+        return result;
+    };
+
+    let drawRectangles = (data: Uint8Array): void => {
         if (!canvas || !canvasCtx) return;
 
         let width = canvas.width;
         let height = canvas.height;
         let barWidth = width / data.length;
-        let centerX = width / 2;
-        let centerY = height / 2;
 
         canvasCtx.clearRect(0, 0, width, height);
 
@@ -212,18 +260,16 @@
             let percent = value / 255;
             let barHeight = height * percent;
 
-            // Calculate the x position. Lower frequencies are drawn in the center, higher frequencies towards the edges.
-            let x = (i < data.length / 2) ? centerX - (barWidth * (data.length / 2 - i)) : centerX + (barWidth * (i - data.length / 2));
-
-            // Calculate the y position. Lower frequencies are drawn higher, higher frequencies are drawn lower.
-            let y = centerY - (barHeight / 2);
+            let x = barWidth * i;
+            let y = height - height * percent;
 
             canvasCtx.fillStyle = 'rgb(' + (percent * 255) + ', 50, 50)';
+
             canvasCtx.fillRect(x, y, barWidth, barHeight);
         }
     };
 
-    let drawRectangles = (data: Uint8Array): void => {
+    let drawRegularPolygon = (data: Uint8Array): void => {
         if (!canvas || !canvasCtx) return;
 
         canvasCtx.lineWidth = 2;
@@ -257,6 +303,7 @@
                 for (var i = 0; i < data.length; i++) {
                     let height: number = ((data[i]) / 255) * (Math.min((canvas.height / 2, canvas.width / 2)) - radius);
                     let rotation: number = (360 * ((i / (data.length - 1) * iteration) + side) / numSides);
+                    let percent = data[i] / 255;
 
                     canvasCtx.save();
 
@@ -268,6 +315,29 @@
                     // }
 
                     const percentage = 0.75;
+
+                    let startColor = Colors.primary[100].replace('#', '');
+                    let endColor = Colors.red[500].replace('#', '');
+
+                    let startRGB = {
+                        r: parseInt(startColor.slice(0, 2), 16),
+                        g: parseInt(startColor.slice(2, 4), 16),
+                        b: parseInt(startColor.slice(4, 6), 16)
+                    };
+
+                    let endRGB = {
+                        r: parseInt(endColor.slice(0, 2), 16),
+                        g: parseInt(endColor.slice(2, 4), 16),
+                        b: parseInt(endColor.slice(4, 6), 16)
+                    };
+
+                    let interpolatedRGB = {
+                        r: Math.round(startRGB.r + percent * (endRGB.r - startRGB.r)),
+                        g: Math.round(startRGB.g + percent * (endRGB.g - startRGB.g)),
+                        b: Math.round(startRGB.b + percent * (endRGB.b - startRGB.b))
+                    };
+
+                    canvasCtx.fillStyle = `rgb(${interpolatedRGB.r}, ${interpolatedRGB.g}, ${interpolatedRGB.b})`;
 
                     if (filled) {
                         canvasCtx.fillRect(-canvasCtx.lineWidth / 2, (radius - (height * percentage)) / percentage, canvasCtx.lineWidth, (height - (height * percentage)) / percentage);
@@ -415,6 +485,12 @@
         <Hexagon class="gap">
             <div class="slider-wrapper">
                 <input aria-label="FFT Size Slider" type="range" min="5" max="15" step="1" bind:value={fftSizeExp} on:input={(e) => {updateFFTSize()}}/>
+            </div>
+        </Hexagon>
+
+        <Hexagon class="gap">
+            <div class="slider-wrapper">
+                <input aria-label="Smoothing Slider" type="range" min="0.01" max="0.1" step="0.01" bind:value={smoothStrength} />
             </div>
         </Hexagon>
 
