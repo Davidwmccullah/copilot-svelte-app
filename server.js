20eb515d085e0053da0c373eb8a5a784457d2f4a
David McCullah <davidwmccullah@gmail.com>
2024-10-03 17:23:48 -0500

    Refactor Server for uptime

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ import { readFileSync } from 'fs';
 import express from 'express';
 import https from 'https';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const PORT = 443;
 const app = express();
+
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Serve static files (e.g., favicon, images) before the SvelteKit handler
 app.use(express.static(path.join(__dirname, 'static')));  // Adjust if needed
