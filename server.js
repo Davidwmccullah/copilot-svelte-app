010b95daeb80f1333c52b648f1a3e48fee4086db
David McCullah <davidwmccullah@gmail.com>
2024-10-04 13:57:19 -0500

    Refactor Server for uptime

--- server.js
+++ server.js
@@ -8,23 +8,19 @@ import { fileURLToPath } from 'url';
 const PORT = 443;
 const app = express();
 
-// Get the directory name of the current module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve static files (e.g., favicon, images) before the SvelteKit handler
-app.use(express.static(path.join(__dirname, 'static')));  // Adjust if needed
+app.use(express.static(path.join(__dirname, 'static')));
 
-// Apply SvelteKit's handler at the end of the middleware chain
 app.use((req, res, next) => {
-  if (req.method === 'GET' || req.method === 'HEAD') {
-    // Make sure the GET/HEAD requests don't have a body
-    if (req.body) {
-      return res.status(400).send('GET/HEAD requests should not have a body');
-    }
+  if ((req.method === 'GET' || req.method === 'HEAD') && req.body) {
+    return res.status(400).send('GET/HEAD requests should not have a body');
   }
-  handler(req, res, next);
+  next();
 });
+
+app.use(handler);
 
 const server = https.createServer({
   key: readFileSync('/etc/ssl/world-of-whimsy.key'),
